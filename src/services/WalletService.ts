87fb6e1e9977f21a6609709c71febaf4a0d3d0c3
David Prieto <dprietoc@gmail.com>
2024-10-16 17:07:27 -0300

    Add wallet status

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -1,16 +1,22 @@
-const WALLET_STATUS = {
+export enum EthereumMethod {
+  CONNECT = "eth_requestAccounts",
+  GET_WALLET = "eth_accounts",
+}
+
+export const WALLET_STATUS = {
   CONNECTED: 'Connected',
   NO_ETHEREUM_BROWSER: 'No Ethereum browser detected, install MetaMask',
   CONNECTION_ERROR: 'Connection error',
+  CONNECT_WALLET: 'Connect to your wallet for minting NFTs',
 };
 
-interface ConnectWalletResponse {
+export interface ConnectWalletResponse {
   status: string;
   address: string | null;
   error?: string;
 }
 
-export const connectWallet = async (): Promise<ConnectWalletResponse> => {
+export const requestWallet = async (method: EthereumMethod): Promise<ConnectWalletResponse> => {
   if (!window.ethereum) {
     return {
       address: null,
@@ -19,19 +25,54 @@ export const connectWallet = async (): Promise<ConnectWalletResponse> => {
   }
 
   try {
-    const addressArray = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-
-    return {
-      status: WALLET_STATUS.CONNECTED,
-      address: addressArray[0],
-    };
+    const addressArray = await window.ethereum.request({method});
+    if (!addressArray.length) {
+      return {
+        address: null,
+        status: WALLET_STATUS.CONNECT_WALLET,
+      }
+    } else {
+      return {
+        address: addressArray[0],
+        status: WALLET_STATUS.CONNECTED,
+      };
+    }
   } catch (error) {
     return {
       address: null,
       status: WALLET_STATUS.CONNECTION_ERROR,
       error: error instanceof Error ? error.message : '',
     };
+  }
+};
+
+export const addWalletListener = async (onChange: (response: ConnectWalletResponse) => void) => {
+  if (!window.ethereum) {
+    onChange({
+      address: null,
+      status: WALLET_STATUS.NO_ETHEREUM_BROWSER,
+    });
+  }
+
+  try {
+    window.ethereum.on("accountsChanged", (accounts: string[]) => {
+      if (accounts.length) {
+        onChange({
+          address: accounts[0],
+          status: WALLET_STATUS.CONNECTED,
+        }); 
+      } else {
+        onChange({
+          address: null,
+          status: WALLET_STATUS.CONNECT_WALLET,
+        });
+      }
+    });
+  } catch (error) {
+    onChange({
+      address: null,
+      status: WALLET_STATUS.CONNECTION_ERROR,
+      error: error instanceof Error ? error.message : '',
+    });
   }
 };
